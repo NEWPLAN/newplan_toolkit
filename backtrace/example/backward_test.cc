871ed460011e9b40d23839c23e1b631453895347
NEWPLAN <enwplan001@163.com>
2022-10-06 22:03:24 +0800

    get all thread ids in a process

--- backtrace/example/backward_test.cc
+++ backtrace/example/backward_test.cc
@@ -16,11 +16,53 @@ namespace
 #include <thread>
 #include <vector>
 #include <memory>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dirent.h>
 namespace
 {
+    void visit_all_threads()
+    {
+        std::vector<int> tids;
+        DIR *proc_dir;
+        {
+            char dirname[100] = "/proc/self/task";
+            // snprintf(dirname, sizeof dirname, "/proc/%d/task", getpid());
+            proc_dir = opendir(dirname);
+        }
+
+        if (proc_dir)
+        {
+            /* /proc available, iterate through tasks... */
+            struct dirent *entry;
+            while ((entry = readdir(proc_dir)) != NULL)
+            {
+                if (entry->d_name[0] == '.')
+                    continue;
+
+                int tid = atoi(entry->d_name);
+                tids.push_back(tid);
+
+                /* ... (do stuff with tid) ... */
+            }
+
+            closedir(proc_dir);
+        }
+        else
+        {
+            /* /proc not available, act accordingly */
+        }
+        for (auto tid : tids)
+        {
+            std::cout << "tid: "
+                      << tid << std::endl;
+        }
+    }
     void thread_function(int t_id)
     {
-        std::cout << "Creating thread: " << t_id << std::endl;
+        pid_t pid = getpid();
+        std::cout << "Creating thread: " << t_id
+                  << " in pid: " << pid << std::endl;
         int left_cycle = 10;
         while (true)
         {
@@ -44,6 +86,7 @@ namespace
                                               thread_function(t_id); });
             thread_group.push_back(std::move(p_thread));
         }
+        visit_all_threads();
         for (auto &p_thread : thread_group)
         {
             p_thread->join();
